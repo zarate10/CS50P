74378d25fb91827b8857f259679d7aae862f2f40
zarate10 <zaratelautaro999@gmail.com>
2022-07-21 00:31:48 -0300

    psetX

---
+++ problem-set-3/taqueria.py
@@ -0,0 +1,27 @@
+price_items = {
+    "Baja Taco": 4.00,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
+
+def taqueria():
+
+    total = 0
+    while True:
+
+        try:
+            item = input('Item: ').strip().title()
+            if item in price_items:
+                total += price_items[item]
+                print(f'${total:.2f}')
+        except (EOFError, KeyError):
+            print("", end="\n")
+            quit()
+
+taqueria()
